822de0094967115d2a7a6553fd331265f557bbeb
Techi Samudio <mtsamudiog@gmail.com>
2021-01-19 14:21:13 -0300

    CF-2 Change menu titltes (#1)
    * feat(app): Change files, titles and sentences to a transaction name
    
    * feat(app): Change files, titles and sentences to a limits name
    
    * feat(codestream): codestream commit

--- app/src/main/java/com/collective/finance/ui/transaction/TransactionsFragment.kt
+++ app/src/main/java/com/collective/finance/ui/transaction/TransactionsFragment.kt
@@ -1,4 +1,4 @@
-package com.collective.finance.ui.notifications
+package com.collective.finance.ui.transaction
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.collective.finance.R
 
-class NotificationsFragment : Fragment() {
+class TransactionsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var transactionsViewModel: TransactionsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        transactionsViewModel =
+                ViewModelProvider(this).get(TransactionsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_transactions, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        transactionsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/collective/finance/ui/transaction/TransactionsViewModel.kt
+++ app/src/main/java/com/collective/finance/ui/transaction/TransactionsViewModel.kt
@@ -1,13 +1,13 @@
-package com.collective.finance.ui.notifications
+package com.collective.finance.ui.transaction
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class TransactionsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is transactions Fragment"
     }
     val text: LiveData<String> = _text
 }
